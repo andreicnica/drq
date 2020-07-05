34772dae5b3bf77942eb2a58c77741622b25a605
andreicnica <andreic.nica@gmail.com>
2020-07-05 12:51:06 -0400

    More mem

--- run_exp.sh
+++ run_exp.sh
@@ -2,9 +2,9 @@
 #SBATCH --array=1-NUM_EXPS
 #SBATCH --partition=long  # Ask for unkillable job
 #SBATCH --cpus-per-task=6                    # Ask for 2 CPUs
-#SBATCH --gres=gpu:pascal:1                          # Ask for 1 GPU
-#SBATCH --mem=8Gb                             # Ask for 10 GB of RAM
-#SBATCH --time=23:00:00                        # The job will run for 3 hours
+#SBATCH --gres=gpu:1                          # Ask for 1 GPU
+#SBATCH --mem=24Gb                             # Ask for 10 GB of RAM
+#SBATCH --time=10:00:00                        # The job will run for 3 hours
 #SBATCH -o /network/tmp1/nicaandr/slurm_logs/slurm-%j.out  # Write the log on tmp1
 
 # 1. Load your environment
